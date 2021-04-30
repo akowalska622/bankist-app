82f6303dd33abd6f68894d5944485f3bd151c7e7
akowalska622 <63072419+akowalska622@users.noreply.github.com>
2021-04-30 14:18:23 +0200

    Handle delete

--- script.js
+++ script.js
@@ -62,8 +62,9 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+let loggedUser;
 //////////////////// DISPLAY MOVEMENTS //////////////////
-const displayMovement = (movements) => {
+const displayMovement = ({ movements }) => {
   containerMovements.innerHTML = "";
 
   movements.forEach((mov, idx) => {
@@ -94,6 +95,7 @@ const createUsernames = (accounts) => {
 createUsernames(accounts);
 
 //////////////////// IMPLEMENTING LOGIN //////////////////
+//section completed without help of the instructor or solution
 const findAccount = (username) =>
   accounts.find((acc) => acc.username === username);
 
@@ -101,36 +103,101 @@ const displayMessage = ({ owner }) => {
   labelWelcome.textContent = `Welcome back ${owner.split(" ")[0]}`;
 };
 
-const displayBalance = ({ movements, interestRate }) => {
-  labelBalance.textContent = movements.reduce((a, b) => a + b, 0) + "€";
+const displayBalance = (account) => {
+  account.balance = account.movements.reduce((a, b) => a + b, 0);
+  labelBalance.textContent = account.balance + "€";
 
   labelSumOut.textContent =
-    Math.abs(movements.filter((x) => x < 0).reduce((a, b) => a + b, 0)) + "€";
+    Math.abs(
+      account.movements.filter((x) => x < 0).reduce((a, b) => a + b, 0)
+    ) + "€";
 
   labelSumIn.textContent =
-    movements.filter((x) => x > 0).reduce((a, b) => a + b, 0) + "€";
+    account.movements.filter((x) => x > 0).reduce((a, b) => a + b, 0) + "€";
 
-  labelSumInterest.textContent = movements
+  labelSumInterest.textContent = account.movements
     .filter((x) => x > 0)
-    .map((deposit) => (deposit * interestRate) / 100)
+    .map((deposit) => (deposit * account.interestRate) / 100)
     .filter((int) => int >= 1)
     .reduce((a, b) => a + b, 0);
 };
 
+const updateUI = (user) => {
+  displayMovement(user);
+  displayBalance(user);
+};
+
 const handleLogin = () => {
   const user = findAccount(inputLoginUsername.value);
+  loggedUser = user;
+
   if (user?.pin === +inputLoginPin.value) {
     containerApp.style.opacity = 1;
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur(); //loose focus on input fields
-
     displayMessage(user);
-    displayMovement(user.movements);
-    displayBalance(user);
+    updateUI(user);
   }
 };
 
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
   handleLogin();
+});
+
+//////////////////// IMPLEMENTING TRANSFERS //////////////////
+//section completed without help of the instructor or solution
+
+const handleTransfer = (user) => {
+  let receiver = accounts.find((x) => x.username === inputTransferTo.value);
+  if (!receiver) return;
+  let amount = +inputTransferAmount.value;
+
+  if (
+    receiver &&
+    amount > 0 &&
+    amount < user.balance &&
+    receiver?.username !== user.username
+  ) {
+    user.movements.push(-amount);
+    receiver.movements.push(amount);
+    updateUI(loggedUser);
+  }
+  inputTransferAmount.value = inputTransferTo.value = "";
+  inputTransferAmount.blur();
+  inputTransferTo.blur();
+};
+
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleTransfer(loggedUser);
+});
+
+//////////////////// DELETE ACCOUNT //////////////////
+//section completed without help of the instructor or solution
+//inputCloseUsername
+//inputClosePin
+//btnClose
+
+const handleLogout = () => {
+  loggedUser = "";
+  containerApp.style.opacity = 0;
+};
+
+const handleDelete = () => {
+  if (
+    inputCloseUsername.value === loggedUser.username &&
+    +inputClosePin.value === loggedUser.pin
+  ) {
+    let indexToDelete = accounts.indexOf(loggedUser);
+    accounts.splice(indexToDelete, 1);
+    handleLogout();
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+  inputCloseUsername.focus();
+};
+
+btnClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleDelete();
 });
